0e885763c1e69ca234f46e9593d182be22c4fc19
CollinsMuiruri <wanyekicollins@gmail.com>
2024-11-16 22:52:53 +0300

    Fix url:change mpesa to simona in urls.py

--- simonaapp/urls.py
+++ simonaapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/mpesa/confirmation', views.mpesa_confirmation, name='mpesa_confirmation'),
-    path('api/mpesa/validation', views.mpesa_validation, name='mpesa_validation'),
+    path('api/simona/confirmation', views.mpesa_confirmation, name='mpesa_confirmation'),
+    path('api/simona/validation', views.mpesa_validation, name='mpesa_validation'),
     path('simulate-transaction/', views.simulate_transaction_view, name='simulate_transaction'),
 ]
